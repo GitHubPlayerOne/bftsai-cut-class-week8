9cfda18ccaa7a0a2c3020c05005013209923a320
bf_tsai <tsai19911026@gmail.com>
2023-09-04 02:21:35 +0800

    index mobile building

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,9 +5,9 @@ import { Navigation, Pagination } from 'swiper/modules';
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'vertical',
-    slidesPerView: '4',
+    slidesPerView: 'auto',
     spaceBetween: 24,
-    enabled:false,
+    enabled: false, //let swiper disable
   
     // If we need pagination
     // pagination: {
@@ -23,7 +23,6 @@ const swiper = new Swiper('.swiper', {
   
     // And if we need scrollbar
     scrollbar: {
-      enabled: true, //can't responsive
       el: '.swiper-scrollbar', /* 整個滾動條的class 名稱，不可更改 */
       hide: false, //設置為true 使用戶滑動離開時隱藏滾動條
       dragSize: 'auto', //設定swiper-scrollbar-drag 的長度，單位為px
@@ -32,18 +31,18 @@ const swiper = new Swiper('.swiper', {
     // 使 swiper 可以使用滑鼠滾輪捲動
     mousewheel: true, //can't responsive
     breakpoints: {
-      1400: {
+      992: {
+        enabled: true, //web need reorganize
         direction: 'horizontal',
         slidesPerView: '3',
         spaceBetween: 24,
 
         scrollbar: {
-          enabled: true,
           el: '.swiper-scrollbar', /* 整個滾動條的class 名稱，不可更改 */
           dragSize: 'auto', //設定swiper-scrollbar-drag 的長度，單位為px
           draggable: true, //設置為true 可用滑鼠拖曳滾動條
         },
-      }
+      },
     }
   });
   
