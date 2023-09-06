e5f1790e39ecbd2b3956cce8b960459ca1cd8789
bf_tsai <tsai19911026@gmail.com>
2023-09-06 16:28:02 +0800

    Updates

--- assets/swiper-test-5258fd77.js
+++
@@ -1,1 +0,0 @@
-import"./bootstrap.bundle-e691f586.js";import{S as e}from"./swiper-bundle-ae89c236.js";new e(".swiper",{loop:!0,speed:1e3,autoplay:{delay:1e3,disableOnInteraction:!1},breakpoints:{768:{autoplay:{enabled:!0,delay:5e3}}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
