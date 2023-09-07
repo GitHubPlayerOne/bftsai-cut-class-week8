96a3a62f9dce81a214b4ba8770ffc0111fbe6435
bf_tsai <tsai19911026@gmail.com>
2023-09-07 23:02:51 +0800

    Updates

--- assets/swiper-test-ca1e4f7c.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./swiper-bundle-4d156eeb.js";new e(".swiper",{loop:!0,speed:1e3,autoplay:{delay:1e3,disableOnInteraction:!1},breakpoints:{768:{autoplay:{enabled:!0,delay:5e3}}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
---
+++ assets/swiper-test-fe9e7a62.js
@@ -0,0 +1,1 @@
+import{S as e}from"./swiper-bundle-c9456a7a.js";new e(".swiper",{loop:!0,speed:1e3,autoplay:{delay:1e3,disableOnInteraction:!1},breakpoints:{768:{autoplay:{enabled:!0,delay:5e3}}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
