3f17dd2f9b381d7a9440af23d412d21fc62d6531
bf_tsai <tsai19911026@gmail.com>
2023-09-08 20:00:28 +0800

    Updates

---
+++ assets/news-223a8476.js
@@ -0,0 +1,1 @@
+import"./bootstrap.bundle-01a3ce62.js";$(document).ready(function(){$(".page-link").click(function(e){e.preventDefault(),$(this).addClass("c-page-active").parent().siblings().find(".page-link").removeClass("c-page-active")}),$(".detail-arrow").click(function(e){e.preventDefault(),$("h1").removeClass("mb-lg-15").addClass("mb-lg-10")})});
